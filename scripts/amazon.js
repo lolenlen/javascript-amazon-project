3203f16d281c93f01c1a62792df8a2150846b83d
len <laurentplassart2003@gmail.com>
2023-09-23 15:11:32 +0200

    added message added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -43,7 +43,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart  js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -93,6 +93,12 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add('added-to-cart-visible');
 
       console.log(cartQuantity);
       console.log(cart);
