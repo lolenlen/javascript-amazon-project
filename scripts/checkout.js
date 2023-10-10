296e911792c1c8d4cd2f677a8f381d8727cf7fa9
len <laurentplassart2003@gmail.com>
2023-10-10 15:54:47 +0200

    delete products from html with delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,8 +19,7 @@ cart.forEach((cartItem) => {
   });
 
  cartSummaryHTML += `
- <div class="order-summary js-order-summary">
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -94,7 +93,6 @@ cart.forEach((cartItem) => {
           </div>
         </div>
       </div>
-      </div>
         `;
 });
 
@@ -106,5 +104,10 @@ document.querySelector('.js-order-summary')
     link.addEventListener('click', () => {
      const productId = link.dataset.productId;
      removeFromCart(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+        container.remove();
     });
   });
